b836bd1d3704efd4ea104e012eb344ea06e81c5a
Lubomir Bartos <lubomir.bartos@intraworlds.com>
2018-04-18 10:45:28 +0200

    test and checkstyle minor changes

--- test/CoreTest.php
+++ test/CoreTest.php
@@ -19,18 +19,18 @@ final class CoreTest extends TestCase
         $this->_core = new Core(
             new class() implements ApiAdapter {
                 /**
-                  * Method that fetches response from 
-                  * intraworlds REST API from payload
-                  * and chosen method.
-                  * returns response in JSON if there was no error
-                  * throws an Exception when an error occurs
-                  *
-                  * @param string $url     url for api request
-                  * @param string $payload payload for api request
-                  * @param string $method  method for api request
-                  *
-                  * @return string representation of response
-                  */
+                 * Method that fetches response from 
+                 * intraworlds REST API from payload
+                 * and chosen method.
+                 * returns response in JSON if there was no error
+                 * throws an Exception when an error occurs
+                 *
+                 * @param string $url     url for api request
+                 * @param string $payload payload for api request
+                 * @param string $method  method for api request
+                 *
+                 * @return string representation of response
+                 */
                 public function sendRequest($url, $payload, $method):string 
                 {
                     if ($url == 'good') {
@@ -88,7 +88,7 @@ final class CoreTest extends TestCase
         $responseJson = json_encode($response);
 
         $this->assertEquals(
-            '{"responseCode":500,"responseDetail":"bad",'+
+            '{"responseCode":500,"responseDetail":"bad",'.
             '"responseHeaders":{"header1":"detail"}}', $responseJson
         );
         $this->assertInternalType("numeric", $time);
